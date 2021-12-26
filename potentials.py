99f00dd0353fee83f73a9be13b6bd9471077a85c
Chris Battista <c.battista@outlook.com>
2021-12-26 01:39:09 -0500

    potentials

--- potentials.py
+++ potentials.py
@@ -1,7 +1,9 @@
 import numpy as np
 import scipy
+import scipy.spatial
 import torch
 from . import worlds
+
 
 def potential_energy(world, potentials):
     '''
@@ -18,26 +20,39 @@ def potential_energy(world, potentials):
 
     return V
 
+
 def kinetic_energy(world):
     '''
     Calculates kinetic energy of a whole particle system.
     '''
 
-    vel_mags = torch.linalg.norm(world.get_state()[:,worlds.vel[world.spatial_dims]], axis=1)
-    T = torch.sum( ((vel_mags**2) * world.get_state()[:,worlds.mass[world.spatial_dims]] / 2),
-                axis=0)
+    return kinetic_energy_from_state(world.get_state(), world.spatial_dims)
+
+
+def kinetic_energy_from_state(state, spatial_dims):
+    vel_mags = torch.linalg.norm(state[:, worlds.vel[spatial_dims]], axis=1)
+    T = torch.sum(((vel_mags**2) * state[:, worlds.mass[spatial_dims]] / 2),
+                  axis=0)
 
     return T
+
 
 def gravitational_potential_energy(world, G):
     '''Returns the total GPE of a world system based on Newton's law of gravitation and a given G.'''
     state = world.get_state()
-    position = state[:, worlds.pos[world.spatial_dims]]
-    mass = state[:, worlds.mass[world.spatial_dims]]
-    one_over_r = torch.reciprocal(torch.tensor(scipy.spatial.distance.pdist(position.detach())))
-    one_over_r_mat = torch.tensor(scipy.spatial.distance.squareform(one_over_r))
+    return gravitational_potential_energy_from_state(state, world.spatial_dims,
+                                                     G)
+
+
+def gravitational_potential_energy_from_state(state, spatial_dims, G):
+    position = state[:, worlds.pos[spatial_dims]]
+    mass = state[:, worlds.mass[spatial_dims]]
+    one_over_r = torch.reciprocal(
+        torch.tensor(scipy.spatial.distance.pdist(position.detach())))
+    one_over_r_mat = torch.tensor(
+        scipy.spatial.distance.squareform(one_over_r))
     one_over_r_mat = torch.nan_to_num(one_over_r_mat, posinf=0, neginf=0)
-    m_M_over_r_mat = one_over_r_mat * mass * mass[...,None]
+    m_M_over_r_mat = one_over_r_mat * mass * mass[..., None]
     pairwise_U = torch.sum(torch.triu(m_M_over_r_mat))
     U_mat = (-1) * G * pairwise_U
     return torch.sum(U_mat)
